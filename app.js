6b8dd168b55260849b800438b5fbc5b463e026d5
Richard Armuelles <samahel021@gmail.com>
2018-10-27 21:08:25 -0500

    text fixed

--- app.js
+++ app.js
@@ -60,9 +60,9 @@ var languageTranslator = new LanguageTranslatorV3({
 
 bot.onText(/\/help/, (msg) => {
   const chatId = msg.chat.id;
-  const instruction_1 = "<strong>Instrucciones:</strong>" + '\n' + "/aboutCharacters personaje: Puedes obtener la biografía de un personaje, también puedes especificar el universo colocandolo entre parentesis, al igual que el nombre en caso de que el personaje haya sido personificado por varias personas." + '\n' + "Ejemplo: /aboutCharacters Spider-man (Ultimate) o /aboutCharacters Venom (Flash Thompson)";
-  const instruction_2 = '/getComicsWhere personaje: Obten una lista de algunos comics en los que haya participado un personaje.' + '\n' + 'Ejemplo: /getComicsWhere Captain America';
-  const instruction_3 = '/getEventInfo evento: Obten información sobre una historia específica, personajes participantes, creadores y comics en donde se aborte dicha historia.' + '\n' + 'Ejemplo: /getEventInfo civil war';
+  const instruction_1 = "<strong>Instrucciones:</strong>" + '\n' + "/aboutCharacters personaje: Puedes obtener la biografía de un personaje, también puedes especificar el universo colocandolo entre parentesis, al igual que el nombre en caso de que el personaje haya sido personificado por varias personas." + '\n\n' + "Ejemplo: /aboutCharacters Spider-man (Ultimate) o /aboutCharacters Venom (Flash Thompson)";
+  const instruction_2 = '/getComicsWhere personaje: Obten una lista de algunos comics en los que haya participado un personaje.' + '\n\n' + 'Ejemplo: /getComicsWhere Captain America';
+  const instruction_3 = '/getEventInfo evento: Obten información sobre una historia específica, personajes participantes, creadores y comics en donde se aborte dicha historia.' + '\n\n' + 'Ejemplo: /getEventInfo civil war';
 
   bot.sendMessage(chatId, instruction_1,{parse_mode: "HTML"})
   bot.sendMessage(chatId, instruction_2,{parse_mode: "HTML"})
